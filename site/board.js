e3a2aab665dcf0acfe86d7157e0bf2f4c2ea309d
Simon Hall <simonhall-scottlogic@users.noreply.github.com>
2016-09-29 10:51:56 +0100

    fixed bugs in render function and now applying the cell class AND the content class to each cell

--- site/board.js
+++ site/board.js
@@ -8,7 +8,7 @@ function render() {
     for (var x = 1; x <= 3; x++) {
         for (var y = 1; y <= 3; y++) {
             var id = "" + x + y;
-            var className = 'board-cell-' + board[x - 1, y - 1];
+            var className = 'board-cell board-cell-' + board[x - 1][y - 1];
             var el = document.getElementById(id);
             el.className = className;
         }
